e381658782bd7cb3167248649640e83a343aeac0
narges Hajabolhasan <narges.abolhasan@gmail.com>
2021-12-14 22:02:22 +0330

    function array sort added

--- main.js
+++ main.js
@@ -14,4 +14,18 @@ function myfun (array){
     console.log(tree)
 }
 
+function Sorting(array2) {
+    var done = false;
+    while (!done) {
+      done = true;
+      for (var i = 1; i < array2.length; i += 1) {
+        if(array2[i - 1]===-1){
+         arr[i]=array2[i-1]
+        }else if (array2[i - 1] > array2[i]) {
+          done = false;
+          var tmp = array2[i - 1];
+          array2[i - 1] = array2[i];
+          array2[i] = tmp;
+        }
+
 
